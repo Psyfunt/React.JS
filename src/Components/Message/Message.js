44db89fdd1f24afede80201d172e0e602ea6f0d7
PsyFunt <psyfunt@gmail.com>
2021-10-29 20:03:14 +0200

    Home_Work_2

---
+++ src/Components/Message/Message.js
@@ -0,0 +1,13 @@
+import './Messages.scss';
+
+export const Message = ({messageList}) => {
+    return (
+        messageList.map((message, index) =>
+            <div key={index}  className='message'>
+                <h3 className='message-author'>{message.user}:</h3>
+                <p className='message-text' >{message.text}</p>
+            </div>)
+    );
+}
+
+export default Message;
